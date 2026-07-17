eb2b90d32a1d15fafa5fda2481baf7cc4a3dabce
deevvv2401 <devcody2401@gmail.com>
2026-07-17 22:33:30 +0530

    Day-12 Update

--- Day-Wise-Learning/Day-12-Strings/Homewrok_D12_Qts/Q1.java
+++ Day-Wise-Learning/Day-12-Strings/Homewrok_D12_Qts/Q1.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class Commulativelength1 {
+public class Q1 {
     // public static void main(String[] args) {
         
     //     Scanner sc = new Scanner(System.in);
--- Day-Wise-Learning/Day-12-Strings/Homewrok_D12_Qts/Q3.java
+++ Day-Wise-Learning/Day-12-Strings/Homewrok_D12_Qts/Q3.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class userName {
+public class Q3 {
     public static void main(String[] args) {
          
         Scanner sc = new Scanner(System.in);
