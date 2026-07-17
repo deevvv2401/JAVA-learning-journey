323b8cdc58ed5ad2585b808b04564e2f04767b59
deevvv2401 <devcody2401@gmail.com>
2026-07-17 22:09:54 +0530

    Day-13 Update

--- Day-Wise-Learning/Day-13-StringBuilder/Homework_D13_Qts/ComulativeLength0.java
+++ Day-Wise-Learning/Day-13-StringBuilder/Homework_D13_Qts/ComulativeLength0.java
@@ -2,7 +2,7 @@
 
 import java.util.*;
 
-public class ComulativeLength{
+public class ComulativeLength0{
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
